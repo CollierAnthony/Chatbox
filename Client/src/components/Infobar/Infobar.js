d3d55070fd1f1d41cf50b1bd367b79df4372ee27
Anthony Collier <anthonycollier@MacBook-Pro-de-Anthony.local>
2021-11-16 15:45:07 +0100

    added alt attribute

--- Client/src/components/Infobar/Infobar.js
+++ Client/src/components/Infobar/Infobar.js
@@ -6,11 +6,11 @@ import './Infobar.css'
 const InfoBar = ({ room }) => (
     <div className='infoBar'>
         <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} />
+            <img className="onlineIcon" alt="" src={onlineIcon} />
             <h3>{room}</h3>
         </div>
         <div className="rightInnerContainer">
-            <a href="/"> <img src={closeIcon} alt="close" /></a>
+            <a href="/"> <img src={closeIcon} alt="" /></a>
         </div>
     </div>
 )
